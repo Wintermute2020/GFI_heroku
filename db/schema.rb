ed96f76ce4bccd2d373890523e0a3193ac7c8461
portOdin <dominic.n@me.com>
2014-06-30 20:40:08 -0400

    going to add extra fields (address, cost, in return)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140630230949) do
+ActiveRecord::Schema.define(version: 20140701001038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,10 @@ ActiveRecord::Schema.define(version: 20140630230949) do
     t.float    "longitude"
     t.integer  "user_id"
     t.string   "category"
+    t.text     "give"
+    t.string   "city"
+    t.string   "address"
+    t.string   "state"
   end
 
   create_table "pictures", force: true do |t|
