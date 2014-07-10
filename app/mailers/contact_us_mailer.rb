6d70f1b8d0ea8074148a9b6ad57564d83cb5fd4b
portOdin <dominic.n@me.com>
2014-07-09 20:56:25 -0400

    contact form working. cleaning up failed attempts.

--- app/mailers/contact_us_mailer.rb
+++
@@ -1,9 +0,0 @@
-class ContactUsMailer < ActionMailer::Base
-  default from: "blur606@gmail.com"
-
-  def contact_us_message(user)
-
-	  mail :to => user, :from => "blur606@gmail.com", :subject => "Subject line"
-
-  end
-end
